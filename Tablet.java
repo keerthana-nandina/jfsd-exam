24a9d77be4d2b901a9e50b43ceaab88fe5f12dc7
keerthana-nandina <162539212+keerthana-nandina@users.noreply.github.com>
2024-12-07 14:41:19 +0530

    Create Tablet.java

---
+++ Tablet.java
@@ -0,0 +1,11 @@
+package com.klef.jfsd.exam;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Tablet extends Device {
+    private double screenSize;
+    private int batteryLife;
+
+    // Getters and Setters
+}
