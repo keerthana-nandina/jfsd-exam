cf0d984ce892fc66b4fe3654a2ca458dd773609b
keerthana-nandina <162539212+keerthana-nandina@users.noreply.github.com>
2024-12-07 14:40:47 +0530

    Create Smartphone.java

---
+++ Smartphone.java
@@ -0,0 +1,11 @@
+package com.klef.jfsd.exam;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Smartphone extends Device {
+    private String operatingSystem;
+    private int cameraResolution;
+
+    // Getters and Setters
+}
